79f43dd535813738975b6028d69bf6c6866da683
Hazana <hazanasec@gmail.com>
2021-03-13 17:36:07 +0000

    update ua

--- arjun/core/utils.py
+++ arjun/core/utils.py
@@ -253,7 +253,7 @@ def prepare_requests(args):
     returns list (of targs)
     """
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate',
